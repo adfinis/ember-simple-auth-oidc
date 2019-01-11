92320e592dbed6d07f2e1d0c46ccd50276dc022c
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2019-01-11 16:44:46 +0100

    remove no longer needed token body in dummy

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -4,15 +4,9 @@ export default function() {
   this.timing = 0;
 
   this.post("/realms/test-realm/protocol/openid-connect/token", function() {
-    let tokenBody = btoa(
-      JSON.stringify({
-        exp: Math.round(new Date().getTime() + 30 * 60)
-      })
-    );
-
     return {
-      access_token: `access.${tokenBody}.token`,
-      refresh_token: `refresh.${tokenBody}.token`
+      access_token: "access.token",
+      refresh_token: "refresh.token"
     };
   });
 
