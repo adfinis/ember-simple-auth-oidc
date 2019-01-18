33e191d145140a5d2e0e62c202754cba715a084b
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2019-01-18 11:10:33 +0100

    remove optional state from end-session redirect

--- addon/mixins/oidc-end-session-route-mixin.js
+++ addon/mixins/oidc-end-session-route-mixin.js
@@ -1,35 +1,22 @@
 import Mixin from "@ember/object/mixin";
 import { inject as service } from "@ember/service";
 import config from "ember-simple-auth-oidc/config";
-import v4 from "uuid/v4";
 
 const { host, endSessionEndpoint, logoutRoute } = config;
 
 export default Mixin.create({
   session: service(),
 
-  async afterModel(
-    _,
-    {
-      queryParams: { state }
-    }
-  ) {
+  // eslint-disable-next-line no-unused-vars
+  afterModel(model) {
     if (!endSessionEndpoint) {
-      this.session.invalidate();
-    }
-
-    if (state) {
-      return this._handleCallback(state);
+      return this.session.invalidate();
     }
 
     return this._handleRedirect();
   },
 
   _handleRedirect() {
-    const state = v4();
-
-    this.session.set("data.state", state);
-
     const redirectUri = `${location.protocol}//${location.host}${logoutRoute}`;
     const idToken = this.session.get("data.authenticated.id_token");
 
@@ -38,16 +25,7 @@ export default Mixin.create({
     location.replace(
       `${host}${endSessionEndpoint}?` +
         `id_token_hint=${idToken}&` +
-        `post_logout_redirect_uri=${redirectUri}&` +
-        `state=${state}`
+        `post_logout_redirect_uri=${redirectUri}`
     );
-  },
-
-  _handleCallback(state) {
-    if (state !== this.session.get("data.state")) {
-      throw new Error("End session state did not match");
-    }
-
-    this.session.set("data.state", undefined);
   }
 });
