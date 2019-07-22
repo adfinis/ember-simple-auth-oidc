e197b8361f4e8cfa5d553afc32f7e5bfe760c69c
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2019-07-22 14:45:58 +0200

    fix(dummy-app): fix queryParams handling in dummy
    The dummy was not adapted to the transition changes in which the
    queryParams are set on `transition.to`.

--- tests/dummy/app/routes/oidc.js
+++ tests/dummy/app/routes/oidc.js
@@ -1,12 +1,10 @@
 import Route from "@ember/routing/route";
 
 export default Route.extend({
-  redirect(
-    _,
-    {
-      queryParams: { redirect_uri, state }
-    }
-  ) {
+  redirect(_, transition) {
+    const { redirect_uri, state } = transition.to
+      ? transition.to.queryParams
+      : transition.queryParams;
     window.location.replace(`${redirect_uri}?code=123456789&state=${state}`);
   }
 });
