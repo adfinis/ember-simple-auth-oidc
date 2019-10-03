cff5831c97213ae242d742423101ff05f8375d14
Yelinz <yelin.zhang@adfinis-sygroup.ch>
2019-10-03 15:30:04 +0200

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
   env: {
     browser: true
   },
-  rules: {},
+  rules: {
+    "ember/no-new-mixins": "warn"
+  },
   overrides: [
     // node files
     {
@@ -34,8 +36,7 @@ module.exports = {
         "tests/dummy/app/**"
       ],
       parserOptions: {
-        sourceType: "script",
-        ecmaVersion: 2015
+        sourceType: "script"
       },
       env: {
         browser: false,
