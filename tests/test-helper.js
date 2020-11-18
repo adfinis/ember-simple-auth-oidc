65204cf441a3fc007201c864abab9df0bad090b3
Jonas Metzener <jonas.metzener@adfinis.com>
2020-11-18 18:04:52 +0100

    fix(deps): update ember and other dependencies

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,7 @@
 import { setApplication } from "@ember/test-helpers";
+import Application from "dummy/app";
+import config from "dummy/config/environment";
 import { start } from "ember-qunit";
-
-import Application from "../app";
-import config from "../config/environment";
 
 setApplication(Application.create(config.APP));
 
