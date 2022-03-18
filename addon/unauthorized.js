4ccc20858b7c37021acfa804818bc8fa8dd65c55
Jonas Metzener <jonas.metzener@adfinis.com>
2022-03-18 13:11:08 +0100

    chore(macros): use embroider macros to detect testing env

--- addon/unauthorized.js
+++ addon/unauthorized.js
@@ -1,4 +1,5 @@
 import { getOwner } from "@ember/application";
+import { isTesting, macroCondition } from "@embroider/macros";
 
 import { getConfig } from "ember-simple-auth-oidc/config";
 import getAbsoluteUrl from "ember-simple-auth-oidc/utils/absoluteUrl";
@@ -8,12 +9,12 @@ export default function handleUnauthorized(session) {
     session.set("data.nextURL", location.href.replace(location.origin, ""));
     session.invalidate();
 
-    const owner = getOwner(session);
-
-    if (
-      owner.resolveRegistration("config:environment").environment !== "test"
-    ) {
-      location.replace(getAbsoluteUrl(getConfig(owner).afterLogoutUri || ""));
+    if (macroCondition(isTesting())) {
+      // don't redirect in tests
+    } else {
+      location.replace(
+        getAbsoluteUrl(getConfig(getOwner(session)).afterLogoutUri || "")
+      );
     }
   }
 }
