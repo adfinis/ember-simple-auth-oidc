7d29652ee01f49fffa138d2b83793ebdc5515630
Falk <falk.neumann@adfinis.com>
2023-04-20 13:52:51 +0200

    chore: upgrade to ember-simple-auth v5
    BREAKING CHANGE: minimum node version >=16

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,12 +3,7 @@
 const EmberAddon = require("ember-cli/lib/broccoli/ember-addon");
 
 module.exports = function (defaults) {
-  const app = new EmberAddon(defaults, {
-    // Add options here
-    "ember-simple-auth": {
-      useSessionSetupMethod: true,
-    },
-  });
+  const app = new EmberAddon(defaults, {});
 
   /*
     This build file specifies the options for the dummy test app of this
