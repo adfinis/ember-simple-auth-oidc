2db3d5be7450dffcc6ffd977654410d1235bc19b
Jonas Metzener <jonas.metzener@adfinis.com>
2023-04-20 13:04:55 +0200

    chore(deps): update dependencies
    BREAKING CHANGE: Drop support for Ember v3, Node < v18 and
    `ember-simple-auth` v4.

--- addon/adapters/oidc-json-api-adapter.js
+++ addon/adapters/oidc-json-api-adapter.js
@@ -1,5 +1,5 @@
-import JSONAPIAdapter from "@ember-data/adapter/json-api";
 import { inject as service } from "@ember/service";
+import JSONAPIAdapter from "@ember-data/adapter/json-api";
 import { handleUnauthorized } from "ember-simple-auth-oidc";
 
 export default class OIDCJSONAPIAdapter extends JSONAPIAdapter {
