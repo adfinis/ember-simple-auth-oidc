347d034802cf6d4566b156ebf80ff3e19a4daaa0
Jonas Metzener <jonas.metzener@adfinis.com>
2023-06-21 12:01:49 +0200

    chore(deps): update dependencies
    BREAKING CHANGE: Drop support for `ember-simple-auth` v5.

--- tests/dummy/mirage/scenarios/default.js
+++ tests/dummy/mirage/scenarios/default.js
@@ -1,9 +1,4 @@
 export default function (server) {
-  /*
-    Seed your development database using your factories.
-    This data will not be loaded in your tests.
-  */
-
   server.create("user", { username: "user1", email: "user1@example.com" });
   server.create("user", { username: "user2", email: "user2@example.com" });
 }
--- tests/dummy/mirage/serializers/application.js
+++ tests/dummy/mirage/serializers/application.js
@@ -1,1 +1,3 @@
-export { JSONAPISerializer } from "miragejs";
+import { JSONAPISerializer } from "miragejs";
+
+export default JSONAPISerializer.extend({});
