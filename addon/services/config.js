adfinis/ember-simple-auth-oidc

commit f31c48e245691b1fae2507ac4c629bca65127458

fix(autodiscovery): await fetching config in beforeModel and afterModel

addon/services/config.js
@@ -25,7 +25,8 @@ const defaultConfig = {
 };
 
 const aliases = {
-  authEndpoint: "authorizationEndpoint",
+  // "originalKey": "aliasKey"
+  authorizationEndpoint: "authEndpoint",
 };
 
 export const applyAliases = (config) => {
@@ -50,12 +51,8 @@ export default class ConfigurationService extends Service {
   constructor(...args) {
     super(...args);
 
-    const envConfig =
-      getOwner(this).resolveRegistration("config:environment")[
-        "ember-simple-auth-oidc"
-      ];
-    if (envConfig) {
-      this.resolvedConfig = applyAliases(camelizeObjectKeys(envConfig));
+    if (this.environment) {
+      this.resolvedConfig = applyAliases(camelizeObjectKeys(this.environment));
     }
 
     this.configKeys.forEach((key) => {
@@ -65,6 +62,12 @@ export default class ConfigurationService extends Service {
         },
       });
     });
+  }
+
+  get environment() {
+    return getOwner(this).resolveRegistration("config:environment")[
+      "ember-simple-auth-oidc"
+    ];
   }
 
   @cached
@@ -85,8 +88,12 @@ export default class ConfigurationService extends Service {
   }
 
   async loadConfig() {
-    if (!this.hasEndpointsConfigured) {
-      await this._fetchAuthConfiguration.perform();
+    if (this.environment.forceAutodiscovery || !this.hasEndpointsConfigured) {
+      if (this._fetchAuthConfiguration.isRunning) {
+        await this._fetchAuthConfiguration.last;
+      } else {
+        await this._fetchAuthConfiguration.perform();
+      }
     }
   }
 
@@ -95,7 +102,7 @@ export default class ConfigurationService extends Service {
    * SPEC: https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfig
    */
   @lastValue("_fetchAuthConfiguration") fetchedConfig;
-  _fetchAuthConfiguration = task(async () => {
+  _fetchAuthConfiguration = task({ drop: true }, async () => {
     if (this._fetchAuthConfiguration.lastSuccessful) {
       return this.fetchedConfig;
     }

tests/unit/services/config-test.js
@@ -30,10 +30,10 @@ module("Unit | Service | config", function (hooks) {
 
   test("alias configuration keys", function (assert) {
     const aliased = applyAliases({
-      authEndpoint: "auth",
+      authorizationEndpoint: "auth",
     });
 
-    assert.strictEqual(aliased.authorizationEndpoint, "auth");
+    assert.strictEqual(aliased.authEndpoint, "auth");
   });
 
   test("fetch configuration if necessary keys are not given", async function (assert) {

addon/routes/oidc-authentication.js
@@ -67,7 +67,9 @@ export default class OIDCAuthenticationRoute extends Route {
    * @param {String} transition.to.queryParams.state The state given by the identity provider
    */
   async afterModel(_, transition) {
-    if (!this.config.authEndpoint) {
+    await this.config.loadConfig();
+
+    if (!this.config.hasEndpointsConfigured) {
       throw new Error(
         "Please define all OIDC endpoints (auth, token, logout, userinfo)",
       );

tests/dummy/config/environment.js
@@ -20,6 +20,7 @@ module.exports = function (environment) {
       loginHintName: "custom_login_hint",
       expiresIn: 60000, // Short expire time (60s) for testing purpose
       refreshLeeway: 1000,
+      forceAutodiscovery: false,
     },
 
     EmberENV: {

commit 7ea3bd675fde128b4d0237e9d1676f3acbf0816a

fix(config): fix configuration of `authEndpoint`
If using the standard configuration (without using auto discovery) the
`authEndpoint` was wrongly set to `undefined` in the config service
because it was overwritten by the value of `authorizationEndpoint` which
is not defined using the standard configuration pattern.

We now make sure to only apply aliases if there is a) a value to be
applied and b) no value present on the key to be written.

This fixes a regression introduced in #1122.

addon/services/config.js
@@ -31,7 +31,11 @@ const aliases = {
 
 export const applyAliases = (config) => {
   Object.entries(aliases).forEach(([key, alias]) => {
-    config[alias] = config[key];
+    const value = config[key];
+
+    if (value && !config[alias]) {
+      config[alias] = value;
+    }
   });
 
   return config;

tests/unit/services/config-test.js
@@ -29,11 +29,15 @@ module("Unit | Service | config", function (hooks) {
   });
 
   test("alias configuration keys", function (assert) {
-    const aliased = applyAliases({
+    const aliased1 = applyAliases({
       authorizationEndpoint: "auth",
     });
+    const aliased2 = applyAliases({
+      authEndpoint: "auth",
+    });
 
-    assert.strictEqual(aliased.authEndpoint, "auth");
+    assert.strictEqual(aliased1.authEndpoint, "auth");
+    assert.strictEqual(aliased2.authEndpoint, "auth");
   });
 
   test("fetch configuration if necessary keys are not given", async function (assert) {