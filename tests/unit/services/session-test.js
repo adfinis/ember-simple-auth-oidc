d2e8b2d91e834302dc8591c0c7645aac3608bbf9
Niels Verbeek <niels.verbeek@adfinis.com>
2026-01-27 12:45:25 +0100

    feat(auth): add refresh hooks for before, after, and error handling

--- tests/unit/services/session-test.js
+++ tests/unit/services/session-test.js
@@ -2,6 +2,7 @@ import { set } from "@ember/object";
 import setupMirage from "ember-cli-mirage/test-support/setup-mirage";
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
+import { fake, stub } from "sinon";
 
 module("Unit | Service | session", function (hooks) {
   setupTest(hooks);
@@ -46,6 +47,66 @@ module("Unit | Service | session", function (hooks) {
     await session.refreshAuthentication.perform();
     assert.verifySteps([]);
   });
+
+  test.each(
+    "it can use authentication refresh hooks with/without errors",
+    [
+      { shouldRefresh: false, hasError: false },
+      { shouldRefresh: true, hasError: false },
+      { shouldRefresh: true, hasError: true },
+    ],
+    async function (assert, { shouldRefresh, hasError }) {
+      const session = this.owner.lookup("service:session");
+      session.session.isAuthenticated = true;
+      session.session.content = {
+        authenticated: {
+          expireTime: shouldRefresh ? 1 : new Date().getTime() + 10000,
+        },
+      };
+
+      session.beforeRefreshAuthentication = fake();
+      session.afterRefreshAuthentication = fake();
+      session.onRefreshAuthenticationError = fake();
+
+      if (hasError) {
+        stub(session.session, "authenticate").rejects({
+          errors: [{ test: "error" }],
+        });
+      } else {
+        stub(session.session, "authenticate").resolves({ data: true });
+      }
+
+      await session.refreshAuthentication.perform();
+
+      // beforeRefreshAuthentication should always be called.
+      assert.ok(session.beforeRefreshAuthentication.calledOnce);
+      if (hasError) {
+        // afterRefreshAuthentication should not be called if there was an error.
+        assert.notOk(session.afterRefreshAuthentication.calledOnce);
+        assert.ok(session.onRefreshAuthenticationError.calledOnce);
+        // onRefreshAuthenticationError receives the error thrown as an argument.
+        assert.deepEqual(
+          session.onRefreshAuthenticationError.firstCall.args[0],
+          {
+            errors: [{ test: "error" }],
+          },
+        );
+      } else {
+        // onRefreshAuthenticationError should not be called if there was no error.
+        assert.notOk(session.onRefreshAuthenticationError.called);
+        assert.ok(session.afterRefreshAuthentication.calledOnce);
+        // afterRefreshAuthentication receives the response as an argument.
+        assert.deepEqual(
+          session.afterRefreshAuthentication.firstCall.args[0],
+          shouldRefresh
+            ? {
+                data: true,
+              }
+            : null,
+        );
+      }
+    },
+  );
 
   test("it can compute authentication headers", function (assert) {
     const session = this.owner.lookup("service:session");
