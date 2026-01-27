8f00e05d75c3563e5ba5285fa1ae1b3759bbfb2d
Niels Verbeek <niels.verbeek@adfinis.com>
2026-01-27 15:11:16 +0100

    chore(deps): add ember-sinon-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,12 +1,14 @@
 import { setApplication } from "@ember/test-helpers";
 import Application from "dummy/app";
 import config from "dummy/config/environment";
-import { start, setupEmberOnerrorValidation } from "ember-qunit";
+import { setupEmberOnerrorValidation, start } from "ember-qunit";
 import { loadTests } from "ember-qunit/test-loader";
+import setupSinon from "ember-sinon-qunit";
 import * as QUnit from "qunit";
 import { setup } from "qunit-dom";
 
 setApplication(Application.create(config.APP));
+setupSinon();
 
 setup(QUnit.assert);
 
